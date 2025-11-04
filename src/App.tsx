7e06f849fa58ad6bb76752c3a27b80f6c6bd236a
Md Sajedul Islam <mdsajedulra@gmail.com>
2025-11-04 10:05:57 +0600

    inital final

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
-
 import "./App.css";
 
-
-import Dashboard from "./pages/Dashboard";
+// import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
     <>
-      <div>
-        <Dashboard />
-      </div>
+      <div></div>
     </>
   );
 }
